32668b6fe98fda6990c1b69901df7050a5649004
Jebakani <vjishwarya@gmail.com>
2021-07-13 15:37:27 +0530

    [JEBAKANI] Creating method to check mandatory condition in email

---
+++ Email/EmailValidity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Email
+{
+    class EmailValidity
+    {
+        public void EmailCheck()
+        {
+            string pattern = @"^[a-zA-Z]{3}@[a-zA-Z]+\.[a-z]{2,3}$";
+            string[] mail = { "abc@gmail.com","xyzgmail@.com" };
+            Regex regex = new Regex(pattern);
+            for (int i = 0; i < mail.Length; i++)
+            {
+                if (regex.IsMatch(mail[i]))
+                {
+                    Console.WriteLine("Valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid");
+                }
+            }
+           
+        }
+    }
+}
--- Email/Program.cs
+++ Email/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 
-namespace PinCode
+namespace Email
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+            EmailValidity email = new EmailValidity();
+            email.EmailCheck();
+            Console.Read();
         }
     }
 }
