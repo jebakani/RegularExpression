bd829f2a06f1dfd7283f6d26606feba8ef59e141
Jebakani <vjishwarya@gmail.com>
2021-07-13 19:56:35 +0530

    [JEBAKANI] Checking for optional top level domain

--- Email/EmailValidity.cs
+++ Email/EmailValidity.cs
@@ -11,8 +11,8 @@ namespace Email
     {
         public void EmailCheck()
         {
-            string pattern = @"^[a-zA-Z]{3}([\- \+ _\.]*[a-zA-Z0-9]+)*@[a-zA-Z0-9]+\.[a-z]{2,3}";
-            string[] mail = { "abc@gmail.com","abcdf2.com","abc+h@tvs.com", "abc@1.com" , "abc-100@yahoo.com", "abc+100@gmail.com" };
+            string pattern = @"^[a-zA-Z]{3}([\- \+ _\.]*[a-zA-Z0-9]+)*@[a-zA-Z0-9]+\.[a-z]{2,3}(\.[a-zA-Z]{2,4}){0,1}$";
+            string[] mail = { "abc@gmail.com","abcdf2.com","abc+h@tvs.com", "abc@1.com" , "abc-100@yahoo.com", "abc+100@gmail.com.in.in" };
             Regex regex = new Regex(pattern);
             for (int i = 0; i < mail.Length; i++)
             {
