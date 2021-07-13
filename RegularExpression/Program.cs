27af274abe79c292539cc5cf576efe285e398a20
Jebakani <vjishwarya@gmail.com>
2021-07-13 12:24:59 +0530

    [JEBAKANI] Checking pincode is valid or not

--- RegularExpression/Program.cs
+++ RegularExpression/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PinCode;
 
 namespace RegularExpression
 {
@@ -7,6 +8,12 @@ namespace RegularExpression
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Regular Expression");
+            PinCodeCheck pincode = new PinCodeCheck();
+            string[] pinCodes = { "400088", "A400088", "400088B" };
+            for (int i = 0; i < pinCodes.Length; i++)
+            {
+                pincode.CheckPin(pinCodes[i]);
+            }
         }
     }
 }
