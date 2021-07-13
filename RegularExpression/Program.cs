b4b98664d99407915b13cdb0e0ba217374323494
Jebakani <vjishwarya@gmail.com>
2021-07-13 12:30:21 +0530

    [JEBAKANI] Creating method to check pincode with space

--- RegularExpression/Program.cs
+++ RegularExpression/Program.cs
@@ -9,7 +9,7 @@ namespace RegularExpression
         {
             Console.WriteLine("Welcome to Regular Expression");
             PinCodeCheck pincode = new PinCodeCheck();
-            string[] pinCodes = { "400088", "A400088", "400088B" };
+            string[] pinCodes = { "400088", "A400088", "400088B","400 088", "400A088" };
             for (int i = 0; i < pinCodes.Length; i++)
             {
                 pincode.CheckPin(pinCodes[i]);
